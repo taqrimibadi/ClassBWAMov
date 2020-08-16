5e4a1e4ba34d81cc809caca0d78bc5317ae8eff4
taqrimibadi <taqrimibadi19@gmail.com>
2020-08-16 11:11:12 +0700

    menambahkan scan qr

---
+++ app/src/main/java/com/taqrim/bwamov/MyWalletActivity.kt
@@ -0,0 +1,11 @@
+package com.taqrim.bwamov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyWalletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_wallet2)
+    }
+}
