b6db1335002948d4c2ef775427ec913f3268fc1d
taqrimibadi <taqrimibadi19@gmail.com>
2020-08-16 11:04:39 +0700

    initial commit

--- app/src/main/java/com/taqrim/bwamov/home/setting/SettingFragment.kt
+++ app/src/main/java/com/taqrim/bwamov/home/setting/SettingFragment.kt
@@ -2,9 +2,14 @@ package com.taqrim.bwamov.home.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.taqrim.bwamov.MyWalletActivity
+import com.taqrim.bwamov.R
 import com.taqrim.bwamov.utils.Preferences
 import kotlinx.android.synthetic.main.fragment_dashboard2.tv_nama
 import kotlinx.android.synthetic.main.fragment_setting.*
@@ -34,6 +39,14 @@ class SettingFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
