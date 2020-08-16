29d8ef3eb899459631a64c2335df29999b886450
taqrimibadi <taqrimibadi19@gmail.com>
2020-08-16 10:35:47 +0700

    initial commit

--- app/src/main/java/com/taqrim/bwamov/home/setting/SettingFragment.kt
+++ app/src/main/java/com/taqrim/bwamov/home/setting/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.taqrim.bwamov.home.setting
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
@@ -47,6 +48,10 @@ class SettingFragment : Fragment() {
             .load(preferences.getValues("url"))
             .apply(RequestOptions.circleCropTransform())
             .into(iv_profile2)
+
+        tv_my_wallet.setOnClickListener{
+            startActivity(Intent(activity, MyWalletActivity::class.java))
+        }
     }
 
     companion object {
--- app/src/main/java/com/taqrim/bwamov/home/tiket/TiketActivity.kt
+++ app/src/main/java/com/taqrim/bwamov/home/tiket/TiketActivity.kt
@@ -1,7 +1,14 @@
 package com.taqrim.bwamov.home.tiket
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.taqrim.bwamov.R
@@ -35,5 +42,29 @@ class TiketActivity : AppCompatActivity() {
         rc_checkout.adapter = TiketAdapter(dataList) {
 
         }
+
+        iv_close.setOnClickListener {
+            finish()
+        }
+
+        iv_barcode.setOnClickListener {
+            showDialog("Silahkan melakukan scanning pada counter tiket terdekat")
+        }
+    }
+
+    private fun showDialog(title: String) {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.scan_qr)
+        dialog.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val tvDesc = dialog.findViewById(R.id.tv_desc) as TextView
+        tvDesc.text = title
+
+        val btnClose = dialog.findViewById(R.id.btn_close) as Button
+        btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
